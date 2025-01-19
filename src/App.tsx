2a8311f99dd970434d80203627e7704cb9679200
Orbitadoo <juanpaavaldezz@gmail.com>
2025-01-19 17:17:31 -0300

    chore: adding husky, eslint and prettier to the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 function App() {
     return (
         <>
-            <div>
-                <h1>Hi</h1>
-                <h2>Somos verde</h2>
-                <h3>Hi</h3>
-            </div>
             <h1>Hello, world!</h1>
         </>
     );
