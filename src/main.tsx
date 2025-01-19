8dd60161df1503eac475e4ee9818e812d598ebe6
Leonardo Juan Pablo Valdez <105684685+Orbitado@users.noreply.github.com>
2025-01-19 17:22:14 -0300

    Configurar React + Vite para el proyecto (#1)
    * Probando Husky con mensajes bonitos
    
    * chore: adding husky, eslint and prettier to the project.

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
-import App from "./App";
-import "./index.css";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router';
+import App from './App';
+import './index.css';
 
-const root = document.getElementById("root")!;
+const root = document.getElementById('root')!;
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+        </Routes>
+    </BrowserRouter>
 );
