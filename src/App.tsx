1ca5f89e51159facebe490af6459e726cc3c1a08
Orbitadoo <juanpaavaldezz@gmail.com>
2025-01-19 17:14:08 -0300

    Probando Husky con mensajes bonitos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 function App() {
-  return (
-    <>
-      <h1>Hello, world!</h1>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <h1>Hi</h1>
+                <h2>Somos verde</h2>
+                <h3>Hi</h3>
+            </div>
+            <h1>Hello, world!</h1>
+        </>
+    );
 }
 
 export default App;
