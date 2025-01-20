95e6fc18e7bb6bcd95254e8fc12ca153074b9a82
Orbitadoo <juanpaavaldezz@gmail.com>
2025-01-20 08:53:27 -0300

    chore: adding flowbite and refactoring the ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { LoginPage } from '@/pages/Auth/LoginPage';
+import { DarkThemeToggle, Flowbite } from 'flowbite-react';
 
 function App() {
     return (
-        <>
+        <Flowbite>
+            <DarkThemeToggle />
             <LoginPage />
-        </>
+        </Flowbite>
     );
 }
 
