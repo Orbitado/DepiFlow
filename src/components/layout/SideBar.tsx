f971c6f03aa375090050f3cef5929cf84ae354ac
Emanuel <emanuelrojasguindan@gmail.com>
2025-01-20 12:51:34 -0300

    feat: added links to sidebar

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -8,31 +8,40 @@ import {
     HiUser,
     HiViewBoards,
 } from 'react-icons/hi';
+import { Link } from 'react-router-dom';
 
 function SideBarLayout() {
     return (
         <Sidebar aria-label="Sidebar with content separator example" className="flex flex-1">
             <Sidebar.Items>
                 <Sidebar.ItemGroup>
-                    <Sidebar.Item href="#" icon={HiChartPie}>
-                        Panel de Control
-                    </Sidebar.Item>
-                    <Sidebar.Item href="#" icon={HiUser}>
-                        Clientes
-                    </Sidebar.Item>
-                    <Sidebar.Item href="#" icon={HiShoppingBag}>
-                        Citas
-                    </Sidebar.Item>
-                    <Sidebar.Item href="#" icon={HiViewBoards}>
-                        Historial
-                    </Sidebar.Item>
+                    <Link to={'/dashboard'}>
+                        <Sidebar.Item icon={HiChartPie}>Panel de Control</Sidebar.Item>
+                    </Link>
+                    <Link to={'/clients'}>
+                        <Sidebar.Item href="#" icon={HiUser}>
+                            Clientes
+                        </Sidebar.Item>
+                    </Link>
+                    <Link to={'/appointments'}>
+                        <Sidebar.Item href="#" icon={HiShoppingBag}>
+                            Citas
+                        </Sidebar.Item>
+                    </Link>
+                    <Link to={'/history'}>
+                        <Sidebar.Item href="#" icon={HiViewBoards}>
+                            Historial
+                        </Sidebar.Item>
+                    </Link>
                 </Sidebar.ItemGroup>
                 <Sidebar.ItemGroup>
-                    <Sidebar.Item href="#" icon={HiArrowSmRight}>
-                        Sign In
-                    </Sidebar.Item>
+                    <Link to={'/auth/login'}>
+                        <Sidebar.Item href="#" icon={HiArrowSmRight}>
+                            Sign In
+                        </Sidebar.Item>
+                    </Link>
                     <Sidebar.Item href="#" icon={HiArrowSmLeft}>
-                        Sign Up
+                        Sign Out
                     </Sidebar.Item>
                     <Sidebar.Item href="#" icon={BiBuoy}>
                         Help
