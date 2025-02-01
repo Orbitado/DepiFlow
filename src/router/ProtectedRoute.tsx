7804605652e828283dcf64109897cf4726ebb2d7
Orbitadoo <juanpaavaldezz@gmail.com>
2025-02-01 17:12:37 -0300

    feat: adding dark mode

--- src/router/ProtectedRoute.tsx
+++ src/router/ProtectedRoute.tsx
@@ -4,10 +4,20 @@ import { RootState } from '@/redux/store';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import AppSidebar from '@/components/layout/AppSideBar';
+import { useEffect } from 'react';
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
     const { status } = useSelector((state: RootState) => state.auth);
     const location = useLocation();
+    const currentTheme = useSelector((state: RootState) => state.theme.theme);
+
+    useEffect(() => {
+        if (currentTheme === 'dark') {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }, [currentTheme]);
 
     if (status === 'not-authenticated') {
         return <Navigate to="/auth/login" state={{ from: location }} replace />;
